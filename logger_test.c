172562ab36bcf883d2d3800b8aefd99cfe23b53a
Diego Hamilton <diego.hamilton@ufpe.br>
2021-01-13 12:29:53 -0300

    Create debug example
    This commit create a debug example with one thread for the logger
    operation and two channels writing to the logger (each one using one
    thread).

--- logger_test.c
+++ logger_test.c
@@ -1,7 +1,79 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 #include "logger.h"
 
 #define print() printf("%s\r\n", __func__);
+
+op_res_t logger_test_init(void);
+void logger_test_start(char *destination);
+void logger_test_stop(void);
+op_res_t logger_test_write(data_t *data);
+void logger_test_wait(void);
+void logger_test_signal(void);
+
+void write_performed() {
+    print();
+}
+
+static data_buffer_t write_buffer;
+
+#define NO_OF_CHANNELS 8
+
+static base_channel_t channels[NO_OF_CHANNELS];
+base_channel_t *channels_ptr = channels;
+
+static char logger_test_destination[20] = "logger_test_stream";
+
+static base_logger_t logger = {
+    ._init = &logger_test_init,
+    ._start = &logger_test_start,
+    ._stop = &logger_test_stop,
+    ._write = &logger_test_write,
+    ._wait = &logger_test_wait,
+    ._signal = &logger_test_signal,
+    .status = 0,
+    .registered_channels = 0,
+    .channels = &channels_ptr,
+    .no_channels = NO_OF_CHANNELS,
+    .destination = logger_test_destination,
+    .write_buffer = &write_buffer
+};
+
+static char channel1_test_name[10] = "channel1";
+static char channel2_test_name[10] = "channel2";
+
+static base_channel_t channel1;
+static base_channel_t channel2;
+
+/* OS section */
+sem_t write_available;
+pthread_t logger_th;
+pthread_t ch1_th, ch2_th;
+void *logger_thread(void *arg);
+void *channel1_thread(void *arg);
+void *channel2_thread(void *arg);
+/* End of OS section */
+
+int main(void) {
+    logger_init(&logger, NO_OF_CHANNELS);
+    logger_register(&logger, channel1_test_name, &channel1);
+    logger_register(&logger, channel2_test_name, &channel2);
+
+    int err;
+    sem_init(&write_available, 0, 0);
+    err = pthread_create(&logger_th, NULL, logger_thread, NULL);
+    err = pthread_create(&ch1_th, NULL, channel1_thread, NULL);
+    err = pthread_create(&ch2_th, NULL, channel2_thread, NULL);
+
+    pthread_join(ch1_th, NULL);
+    pthread_join(ch2_th, NULL);
+    logger_stop(&logger);
+    pthread_join(logger_th, NULL);
+
+    return 0;
+}
 
 op_res_t logger_test_init(void) {
     print();
@@ -20,57 +92,41 @@ void logger_test_stop(void) {
 
 op_res_t logger_test_write(data_t *data) {
     print();
+    uint8_t *dummy = data->data;
+    printf("channel %d: %d %d %d\r\n", data->id, *dummy, *(dummy + 1), *(dummy + 2));
     return SUCCESS;
 }
 
 void logger_test_wait(void) {
     print();
+    sem_wait(&write_available);
     return;
 }
 
 void logger_test_signal(void) {
     print();
+    sem_post(&write_available);
     return;
 }
 
-static data_buffer_t write_buffer;
+void *logger_thread(void *arg) {
+    logger_start(&logger, logger_test_destination);
+}
 
-#define NO_OF_CHANNELS 8
+void *channel1_thread(void *arg) {
+    uint8_t dummy[3] = {1, 2, 3};
 
-static base_channel_t channels[NO_OF_CHANNELS];
+    for(int i=0; i < 3; i++) {
+        sleep(2);
+        logger_write_async(&logger, channel1.id, dummy, 3, write_performed);
+    }
+}
 
-static char logger_test_destination[20] = "logger_test_stream";
+void *channel2_thread(void *arg) {
+    uint8_t dummy[3] = {4, 5, 6};
 
-static base_logger_t logger = {
-    ._init = &logger_test_init,
-    ._start = &logger_test_start,
-    ._stop = &logger_test_stop,
-    ._write = &logger_test_write,
-    ._wait = &logger_test_wait,
-    ._signal = &logger_test_signal,
-    .status = 0,
-    .registered_channels = 0,
-    .channels = &channels,
-    .no_channels = NO_OF_CHANNELS,
-    .destination = logger_test_destination,
-    .write_buffer = &write_buffer
-};
-
-static char channel1_test_name[10] = "channel1";
-
-static base_channel_t channel1;
-
-int main(void) {
-    logger_init(&logger, NO_OF_CHANNELS);
-    logger_register(&logger, channel1_test_name, &channel1);
-    printf("Channel ID after register: %d\r\n", channel1.id);
-    logger_unregister(&logger, &channel1);
-    printf("Channel ID after unregister: %d\r\n", channel1.id);
-    logger_register(&logger, channel1_test_name, &channel1);
-    printf("Channel ID after register: %d\r\n", channel1.id);
-
-    
-
-
-    return 0;
+    for(int i=0; i < 3; i++) {
+        sleep(2);
+        logger_write_async(&logger, channel2.id, dummy, 3, write_performed);
+    }
 }
