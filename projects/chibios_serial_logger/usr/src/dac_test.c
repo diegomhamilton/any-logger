13e39ab2fd0aa615d4d93b9a369de833d6698421
Diego Hamilton <diego.hamilton@ufpe.br>
2021-01-28 00:31:35 -0300

    Set DAC wave frequency to 1 Hz

--- projects/chibios_serial_logger/usr/src/dac_test.c
+++ projects/chibios_serial_logger/usr/src/dac_test.c
@@ -71,7 +71,7 @@ static const DACConversionGroup dacgrpcfg1 = {
  * GPT6 configuration.
  */
 static const GPTConfig gpt6cfg1 = {
-  .frequency    = 3600U,
+  .frequency    = 360U,
   .callback     = NULL,
   .cr2          = TIM_CR2_MMS_1,    /* MMS = 010 = TRGO on Update Event.    */
   .dier         = 0U
